a883629bac9142ddd45a6049e0369913da484e79
Felipe Abreu <abreufelipe12@hotmail.com>
2025-08-24 18:29:21 -0300

    Criando o efeito de rotação dos cards

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -25,5 +25,20 @@ for(i = 0; i < emojis.length; i++) {
     let box = document.createElement("div");
     box.classList.add("item");
     box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
     document.querySelector(".game").appendChild(box);
 }
+
+function handleClick() {
+    if(openCards.length < 2) {
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if(openCards.length == 2) {
+        setTimeout(checkMatch, 500);
+    }
+}
+
+
+function checkMatch() {}
